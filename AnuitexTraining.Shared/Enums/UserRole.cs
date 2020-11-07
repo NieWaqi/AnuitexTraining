3cc9e86e708640d913adc708e415aeba1aec5249
Leonid Pshenychnyi <peronov.llu@gmail.com>
2020-11-08 00:38:41 +0300

    Update UserRole.cs

--- AnuitexTraining.Shared/Enums/UserRole.cs
+++ AnuitexTraining.Shared/Enums/UserRole.cs
@@ -1,12 +1,1 @@
-﻿namespace AnuitexTraining.Shared.Enums
-{
-    public partial class Enums
-    {
-        public enum UserRole
-        {
-            None = 0,
-            Admin = 1,
-            Client = 2
-        }
-    }
-}
+
